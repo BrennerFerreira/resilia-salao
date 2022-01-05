8bc59809c1b36a5580acb929ea5231bc1d702974
BrennerFerreira <brennercsferreira@gmail.com>
2022-01-05 19:17:36 -0300

    adiciona react router no app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Home } from "./pages/Home";
+import { Usuarios } from "./pages/Usuarios";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/resilia-salao" element={<Home />} />
+        <Route path="/resilia-salao/usuarios" element={<Usuarios />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
