4a9f6266921805ccf5e84a6b5e808f9c1f350872
BrennerFerreira <brennercsferreira@gmail.com>
2022-01-05 19:17:24 -0300

    pagina basica de usuarios

---
+++ src/pages/Usuarios.js
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export const Usuarios = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link to="/resilia-salao">Home</Link>
+    </div>
+  );
+};
