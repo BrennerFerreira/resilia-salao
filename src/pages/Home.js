88b0a2eec425d7c31e7622ebc6094af3ea5d0460
BrennerFerreira <brennercsferreira@gmail.com>
2022-01-12 14:00:46 -0300

    cria home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,29 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import styled from "styled-components";
 import { Statistics } from "../components/home/Statistics";
 import { getApiUrl } from "../utils/getApiUrl";
+
+const Container = styled.div`
+  display: grid;
+  grid-template-areas:
+    ". title title title title ."
+    ". subtitle subtitle subtitle subtitle ."
+    ". statistics statistics statistics statistics .";
+`;
+
+const StyledTitle = styled.h1`
+  grid-area: title;
+`;
+
+const StyledSubtitle = styled.h4`
+  grid-area: subtitle;
+`;
+
+const StyledStatistics = styled.main`
+  margin: 32px 0;
+  grid-area: statistics;
+`;
 
 export const Home = () => {
   const [statistics, setStatistics] = useState({});
@@ -12,6 +34,7 @@ export const Home = () => {
       setLoading(true);
       const url = getApiUrl("/home");
       const statistics = await axios.get(url);
+      console.log(statistics.data);
       setStatistics(statistics.data);
       setLoading(false);
     };
@@ -23,10 +46,19 @@ export const Home = () => {
     return <div>Carregando...</div>;
   };
 
-  return (
-    <div>
-      <h1>Bem-vinde ao sistema de gerenciamento do seu salão de beleza</h1>
-      {loading ? <Loading /> : <Statistics statistics={statistics} />}
-    </div>
+  return loading ? (
+    <Loading />
+  ) : (
+    <Container>
+      <StyledTitle>
+        Bem-vinde ao sistema de gerenciamento do seu salão de beleza
+      </StyledTitle>
+      <StyledSubtitle>
+        Seu salão está incrível! Veja algumas informações:
+      </StyledSubtitle>
+      <StyledStatistics>
+        <Statistics statistics={statistics} />
+      </StyledStatistics>
+    </Container>
   );
 };
